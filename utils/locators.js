395e952cd505f1ab306cb21394bd8a8e94107d09
Alok952 <9992alok@gmail.com>
2025-04-01 19:48:47 +0530

    commited

---
+++ utils/locators.js
@@ -0,0 +1,9 @@
+const locators = {
+  LOGIN_PAGE: {
+    USERNAME_FIELD: "user-name",  // ID should be a simple string without the # symbol for `By.id`
+    PASSWORD_FIELD: "password",
+    LOGIN_BUTTON: "login-button",
+    ERROR_MESSAGE: "error-message-container"
+  }
+};
+export default locators;
